5bf06fbc32f93ec781a1cb3ee6049e1ad27b2ca7
yeeaea <xexun0302@gmail.com>
2023-09-26 15:46:07 +0900

    Initial commit

---
+++ src/main/java/org/online/lms/E1i4Application.java
@@ -0,0 +1,13 @@
+package org.online.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E1i4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(E1i4Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/online/lms/E1i4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.online.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E1i4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
