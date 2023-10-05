e149ab2624e78d39f86e8a522d7c7c52cad94984
yeeaea <xexun0302@gmail.com>
2023-10-05 12:48:50 +0900

    개설강의목록, 강의 계획서

---
+++ src/main/java/org/online/lms/lecture/controller/LectureInfoViewController.java
@@ -0,0 +1,41 @@
+package org.online.lms.lecture.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.online.lms.lecture.domain.LectureInfo;
+import org.online.lms.lecture.dto.LectureInfoViewResponse;
+import org.online.lms.lecture.service.LectureInfoService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/lms")
+public class LectureInfoViewController {
+
+    private final LectureInfoService lectureInfoService;
+
+    // 개설 강의 목록
+    @GetMapping("/online/lecture-all")
+    public String lectureInfoList(Model model) {
+        List<LectureInfoViewResponse> lectureInfo = lectureInfoService.findAll().stream()
+                .map(LectureInfoViewResponse::new)
+                .toList();
+        model.addAttribute("lectureInfo", lectureInfo); // 강의 목록 리스트 저장
+        
+        return "page/lectureInfoList"; // lectureInfoList.html 뷰 조회999
+    }
+
+    // 강의 계획서 조회
+    @GetMapping("/online/lecture-info/{lectureNo}")
+    public String lectureInfo(@PathVariable Long lectureNo, Model model) {
+        LectureInfo lectureInfo = lectureInfoService.findById(lectureNo);
+        model.addAttribute("lectureInfo", new LectureInfoViewResponse(lectureInfo));
+
+        return "page/lectureInfo";
+    }
+}
