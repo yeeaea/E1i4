abd948d56f969758e1c5698f2dbbe4b98dd3d154
yeeaea <xexun0302@gmail.com>
2023-10-05 09:52:05 +0900

    일단 도메인 이렇게 만들었는데 다 이런식으로 만들면 될 것 같습니당....

---
+++ src/main/java/org/online/lms/security/domain/Members.java
@@ -0,0 +1,47 @@
+package org.online.lms.security.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "members")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Members { // 회원 정보 테이블
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_no", updatable = false)
+    private Long memberNo; // 회원 번호
+
+    @Column(name = "login_id", nullable = false)
+    private String loginId; // 로그인 id
+
+    @Column(name = "login_pw", nullable = false)
+    private String loginPw; // 비밀번호
+
+    @Column(name = "member_name", nullable = false)
+    private String memberName; // 이름
+
+    @Column(name = "member_tel")
+    private String memberTel; // 전화번호
+
+    @Column(name = "member_email", nullable = false)
+    private String memberEmail; // 이메일
+
+    @Builder
+    public Members(String loginId,
+                   String loginPw,
+                   String memberName,
+                   String memberTel,
+                   String memberEmail) {
+        this.loginId = loginId;
+        this.loginPw = loginPw;
+        this.memberName = memberName;
+        this.memberTel = memberTel;
+        this.memberEmail = memberEmail;
+    }
+
+}
