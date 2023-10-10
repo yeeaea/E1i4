e25da8f3a2948ac64e8071a5a7c9b27daa6d6b42
yeeaea <xexun0302@gmail.com>
2023-10-10 14:26:50 +0900

    강의 등록 수정 하는 하는 중

--- src/main/resources/static/js/lectureUpdate.js
+++ src/main/resources/static/js/lectureUpdate.js
@@ -1,88 +1,94 @@
+// 페이지가 로드될 때 실행할 함수
+// 페이지가 로드될 때 실행할 함수
 document.addEventListener("DOMContentLoaded", function () {
-    // 강의 등록 폼 제출 기능
-    const addLectureForm = document.querySelector("#addLectureForm");
+    // 강의 추가 기능
+    document.getElementById("lectureAdd").addEventListener("click", function () {
+        // 폼 데이터를 가져오기
+        const lectureForm = document.getElementById("addLectureForm");
+        const formData = new FormData(lectureForm);
 
-    addLectureForm.addEventListener("submit", function (event) {
-        event.preventDefault();
+        // FormData를 JSON으로 변환
+        const jsonObject = {};
+        formData.forEach((value, key) => {
+            jsonObject[key] = value;
+        });
 
-        // 각 입력 필드에서 데이터 수집
-        const lectureYear = document.querySelector("#lectureYear").value;
-        const lectureCourse = document.querySelector("#lectureCourse").value;
-        const lectureTitle = document.querySelector("#lectureTitle").value;
-        const lectureDesc = document.querySelector("#lectureDesc").value;
-        const lectureStartAt = document.querySelector("#lectureStartAt").value;
-        const lectureEndAt = document.querySelector("#lectureEndAt").value;
-
-        // 수집한 데이터를 객체로 만듦
-        const formData = {
-            lectureYear: lectureYear,
-            lectureCourse: lectureCourse,
-            lectureTitle: lectureTitle,
-            lectureDesc: lectureDesc,
-            lectureStartAt: lectureStartAt,
-            lectureEndAt: lectureEndAt
-        };
-
-        // 서버로 데이터를 JSON 형식으로 전송
+        // JSON 데이터를 서버로 전송
         fetch("/admin/api/lectures/add", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify(formData)
+            body: JSON.stringify(jsonObject)
         })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error("서버 응답 오류");
-                }
-                // POST 요청 후 서버에서 리다이렉션을 수행하면 이 코드는 실행되지 않음
-                return response.json();
-            })
+            .then(response => response.json())
             .then(data => {
-                // 성공 시 강의 목록 페이지로 리다이렉션
-                window.location.href = "/admin/online/lecture-all";
-            })
-            .catch(error => {
-                console.error("에러 발생: " + error);
+                if (data.success) {
+                    // 추가 성공 시 필요한 동작 수행
+                    alert("강의 추가 완료");
+                    // 페이지 리로딩
+                    location.reload();
+                } else {
+                    // 추가 실패 시 필요한 동작 수행
+                    alert("강의 추가 실패");
+                }
             });
     });
+
+
+
+    // 강의 삭제 기능
+    // 삭제 버튼 클릭 시 실행될 함수
+    document.getElementById('deleteSelected').addEventListener('click', function () {
+        console.log("Delete button clicked."); // 디버깅 메시지
+        let selectedLectures = document.querySelectorAll('.lecture-checkbox:checked');
+        if (selectedLectures.length === 0) {
+            alert("선택된 강의가 없습니다.");
+            return;
+        }
+
+        // 선택된 강의를 삭제할지 확인하는 팝업을 표시합니다.
+        if (confirm("선택한 강의를 삭제하시겠습니까?")) {
+            // 선택된 강의를 삭제하는 AJAX 요청을 보냅니다.
+            let lectureNos = Array.from(selectedLectures).map(function(checkbox) {
+                return parseInt(checkbox.value);
+            });
+
+            // 서버로 선택된 강의 번호 목록을 JSON 형식으로 변환하여 전송합니다.
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", "/admin/api/lectures/delete", true);
+            xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        // 삭제가 성공한 경우, 화면에서 선택한 강의를 제거합니다.
+                        selectedLectures.forEach(function(checkbox) {
+                            checkbox.closest("tr").remove();
+                        });
+                    } else {
+                        alert("강의 삭제 중 오류가 발생했습니다.");
+                    }
+                }
+            };
+
+            // 선택된 강의 번호 목록을 JSON 형식으로 변환하여 전송합니다.
+            xhr.send(JSON.stringify(lectureNos));
+        }
+    });
+
+
 });
 
-// 강의 수정 기능
-function fillEditForm(row) {
-    let lectureNo = row.getAttribute("data-lecture-no");
-    let form = document.getElementById("addLectureForm");
+// 날짜 선택할 때 달력에서 선택하는 기능
+$(document).ready(function() {
+    $('#lectureStartAt').datepicker({
+        format: 'yyyy-mm-dd',
+        autoclose: true
+    });
 
-    // lectureInfo는 강의 객체를 포함하는 배열
-    let lecture = lectureInfo.find(function (item) {
-        return item.lectureNo === lectureNo;
-    })
-}
-// 강의 삭제 기능
-// 삭제 버튼 클릭 시 실행될 함수
-document.getElementById('deleteSelected').addEventListener('click', function () {
-    const selectedCheckboxes = document.querySelectorAll('.lecture-checkbox:checked');
-    const lectureNos = Array.from(selectedCheckboxes).map(checkbox => checkbox.value);
-
-    if (lectureNos.length === 0) {
-        alert('선택된 강의가 없습니다.');
-        return;
-    }
-
-    // 선택한 강의의 lectureNo 배열을 서버로 전송
-    fetch('/admin/api/lectures/delete', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ lectureNos }),
-    })
-        .then(response => response.json())
-        .then(data => {
-            alert(data); // 서버에서 반환한 메시지를 표시
-            location.reload(); // 페이지 새로 고침
-        })
-        .catch(error => {
-            console.error('삭제 요청 중 오류 발생:', error);
-        });
+    $('#lectureEndAt').datepicker({
+        format: 'yyyy-mm-dd',
+        autoclose: true
+    });
 });
