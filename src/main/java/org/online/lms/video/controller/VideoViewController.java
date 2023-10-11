9269e5650c4848cb74a487a687d862f216939934
javm9 <rjavm999@gmail.com>
2023-10-11 12:30:36 +0900

    content 전체목록 + 수정 + 삭제

---
+++ src/main/java/org/online/lms/video/controller/VideoViewController.java
@@ -0,0 +1,53 @@
+package org.online.lms.video.controller;
+
+
+import org.online.lms.lecture.dto.LectureInfoViewResponse;
+import org.online.lms.video.domain.Content;
+import org.online.lms.video.dto.VideoInfoRequest;
+import org.online.lms.video.dto.VideoInfoViewResponse;
+import org.online.lms.video.service.VideoInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin")
+public class VideoViewController {
+
+    private final VideoInfoService videoInfoService;
+    @Autowired
+    public VideoViewController(VideoInfoService videoInfoService) {
+        this.videoInfoService = videoInfoService;
+    }
+
+
+    @GetMapping("/content")
+    public String lectureInfoList(Model model) {
+
+        // 콘텐츠 리스트
+        List<VideoInfoViewResponse> contentInfo = videoInfoService.findAll().stream()
+                .map(VideoInfoViewResponse::new)
+                .toList();
+        model.addAttribute("contentInfo", contentInfo);
+
+        return "/page/video/videoList"; 
+    }
+
+    @GetMapping("/content-edit")
+    public String contentRegister(Model model) {
+
+        // 콘텐츠 리스트 + 등록 + 수정
+        List<Content> contentList = videoInfoService.findAll();
+        model.addAttribute("contentList", contentList);
+        model.addAttribute("videoInfoRequest", new VideoInfoRequest());
+
+        return "/page/video/videoRegister";
+    }
+
+
+}
