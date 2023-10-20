567df16ad182098d0808fe3145e75e7434a3f346
yeeaea <xexun0302@gmail.com>
2023-10-20 12:33:59 +0900

    , 개설강의 삭제하면 해당 강의번호로 저장된 강의평가, 강의평가 답변도 삭제, 관리자 강의평가

--- src/main/java/org/online/lms/survey/repository/SurveyRepository.java
+++ src/main/java/org/online/lms/survey/repository/SurveyRepository.java
@@ -1,9 +1,12 @@
 package org.online.lms.survey.repository;
 
+import jakarta.transaction.Transactional;
 import org.online.lms.survey.domain.Survey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface SurveyRepository extends JpaRepository<Survey, Long> {
     // 강의 번호와 회원 번호로 중복 강의평가여부 확인
@@ -11,4 +14,7 @@ public interface SurveyRepository extends JpaRepository<Survey, Long> {
     boolean isDuplicate(@Param("lectureNo") Long lectureNo, @Param("memberNo") Long memberNo);
 
 
+    // 여러 강의 번호에 대한 데이터 삭제
+    @Transactional
+    void deleteByLectureNoIn(List<Long> lectureNos);
 }
