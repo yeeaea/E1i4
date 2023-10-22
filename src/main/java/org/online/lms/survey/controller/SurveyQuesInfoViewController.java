0602d0d7348af61b6c439f339f492c93ad1ae8ff
yeeaea <xexun0302@gmail.com>
2023-10-22 16:12:30 +0900

    강의평가문항 뷰 페이징

--- src/main/java/org/online/lms/survey/controller/SurveyQuesInfoViewController.java
+++ src/main/java/org/online/lms/survey/controller/SurveyQuesInfoViewController.java
@@ -4,12 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.online.lms.survey.domain.SurveyQuesInfo;
 import org.online.lms.survey.service.SurveyAnswerService;
 import org.online.lms.survey.service.SurveyQuesInfoService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -20,10 +22,16 @@ public class SurveyQuesInfoViewController {
     private final SurveyAnswerService surveyAnswerService;
     // 강의 평가 문항 목록 조회
     @GetMapping("/ques-info")
-    public String surveyQuesInfoList(Model model) {
+    public String surveyQuesInfoList(@PageableDefault(size = 10) Pageable pageable,
+                                     Model model) {
         // 강의 평가 문항 리스트로 가져오기
-        List<SurveyQuesInfo> surveyQuesInfoList = surveyQuesInfoService.getAllSurveyQuesInfo();
+        Page<SurveyQuesInfo> surveyQuesInfoList = surveyQuesInfoService.getAllSurveyQuesInfoSortedByViewNoWithPaging(pageable);
 
+        int startPage = Math.max(1, surveyQuesInfoList.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(surveyQuesInfoList.getPageable().getPageNumber() + 4, surveyQuesInfoList.getTotalPages());
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("surveyQuesInfoList", surveyQuesInfoList);
 
         return "/page/survey/surveyQuesUpdate";
